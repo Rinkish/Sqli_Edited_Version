51d62269fbf4905191b9a8992fbc0c403c3ccf98
Rinkish <Rinkish.Khera@milwaukeetool.com>
2018-07-22 18:19:55 +0530

    Changes for Less-17 - Less-19

---
+++ sqlilabs/sql-connections/sql-connect-1.php
@@ -0,0 +1,31 @@
+<?php
+
+//including the Mysql connect parameters.
+include("../sql-connections/db-creds.inc");
+@error_reporting(0);
+@$con = mysqli_connect($host,$dbuser,$dbpass);
+// Check connection
+if (!$con)
+{
+    echo "Failed to connect to MySQL: " . mysqli_error($con);
+}
+
+
+    @mysqli_select_db($con, $dbname1) or die ( "Unable to connect to the database: $dbname1".mysqli_error($con));
+
+
+
+
+
+
+$sql_connect_1 = "SQL Connect included";
+
+############################################
+# For Challenge series--- Randomizing the Table names.
+
+?>
+
+
+
+
+ 
---
+++ sqlilabs/sql-connections/sql-connect.php
@@ -0,0 +1,37 @@
+<?php
+
+//including the Mysql connect parameters.
+include("../sql-connections/db-creds.inc");
+@error_reporting(0);
+@$con = mysqli_connect($host,$dbuser,$dbpass);
+// Check connection
+if (!$con)
+{
+    echo "Failed to connect to MySQL: " . mysqli_error($con);
+}
+
+
+    @mysqli_select_db($con, $dbname) or die ( "Unable to connect to the database: $dbname");
+
+
+
+
+
+
+$sql_connect = "SQL Connect included";
+############################################
+# For Less-24
+$form_title_in="Please Login to Continue";
+$form_title_ns="New User";
+$feedback_title_ns="New User";
+$form_title_ns= "Less-24";
+
+############################################
+# For Challenge series--- Randomizing the Table names.
+
+?>
+
+
+
+
+ 
